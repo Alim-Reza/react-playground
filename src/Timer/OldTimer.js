a4289152b0db57921cdf75014409ae7d61ee9c98
Alim-Reza <aarasif2@gmail.com>
2023-03-20 10:43:47 +0600

    1 file modified

--- src/Timer/OldTimer.js
+++ src/Timer/OldTimer.js
@@ -46,7 +46,7 @@ export default function OldTimer() {
     return () => {
       clearInterval(tradeRequestApprovalCountDownInterval);
     };
-  }, [hiddenTime, visibleTime]);
+  }, [hiddenTime, visibleTime, deductLostTime]);
 
   const secondToHHMMSS = (seconds) => {
     return new Date(seconds * 1000).toISOString().substring(11, 11 + 8);
