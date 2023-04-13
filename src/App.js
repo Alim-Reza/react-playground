7f06ff5cdd83f76b000ed1df2e9f9efcbab0790b
Alim-Reza <aarasif2@gmail.com>
2023-04-14 00:53:01 +0600

    1 file modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Timer from './Timer/Timer.js';
 import HoldAndPressButton from './HoldAndPressButton/HoldAndPressButton.js';
 import OldTimer from './Timer/OldTimer.js';
 import CheckboxComponent from './CheckboxComponent/CheckboxComponent.js';
-import TreatManager from './TreatManager/TreatManager.js';
 import FlashyScroll from './FlashyScroll/FlashyScroll.jsx';
 import Navbar from './Navbar/Navbar.js';
 import RegistrationForm from './RegistrationForm/RegistrationForm.js';
@@ -14,10 +13,7 @@ export default function App() {
   return (
     <div>
       {/* <Navbar /> */}
-      {/* <h1>Hello StackBlitz!</h1> */}
       {/* <CheckboxComponent /> */}
-      {/* <p>Start editing to see some magic happen :)</p> */}
-      {/* <TreatManager /> */}
       {/* <FlashyScroll /> */}
       {/* <FixButton /> */}
       {/* <Timer /> */}
