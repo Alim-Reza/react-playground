c59de3084d40df243bd29ebfb1926568e6f58693
Alim-Reza <aarasif2@gmail.com>
2023-05-31 15:17:19 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ export default function App() {
       {/* <OldTimer showHourValue={true} giveTimeInSecond={100} /> */}
       {/* <HoldAndPressButton /> */}
       {/* <RegistrationForm /> */}
-      <ShakeButton />
-      <ProgressBarDiv />
-      <FileDownload
+      {/* <ShakeButton /> */}
+      {/* <ProgressBarDiv /> */}
+      {/* <FileDownload
         url={
           'https://s3.ap-northeast-2.amazonaws.com/new-commerce-dev-new/dev/admin-panel/product/attachment99dc36ce-8b1b-4888-b269-364af9d83a3600613672-b0e7-4006-8f72-dbb82d2eee52.pptx'
         }
-      />
+      /> */}
     </div>
   );
 }
