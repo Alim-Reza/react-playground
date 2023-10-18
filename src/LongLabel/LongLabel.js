f79762b52948f2a2b4a1622100b9afe297fb7580
Alim-Reza <aarasif2@gmail.com>
2023-10-18 09:57:22 +0600

    2 files modified

--- src/LongLabel/LongLabel.js
+++ src/LongLabel/LongLabel.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './LongLabel.css';
 
-export default function LongLabel(props) {
+// export default function LongLabel(props) {
+//   return (
+//     <>
+//       <div className="container">
+//         <div className="scrollable-content">123456789012345678901234567890</div>
+//       </div>
+//     </>
+//   );
+// }
+
+import React, { useEffect, useState } from 'react';
+
+const LongLabel = ({
+  text = '12345678901234567890123456789',
+  width = '100',
+}) => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    if (text && text.length * 8 > width) {
+      setScrolling(true);
+    }
+  }, [text, width]);
+
   return (
-    <>
-      <div className="container">
-        <div className="scrollable-content">123456789012345678901234567890</div>
+    <div className="scroll-text-container" style={{ width: `${width}px` }}>
+      <div className={`scroll-text ${scrolling ? 'scrolling' : ''}`}>
+        {text}
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default LongLabel;
