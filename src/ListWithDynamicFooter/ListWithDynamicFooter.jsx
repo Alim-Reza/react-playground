1f52fac4a48d2d47fb5ff5eaa90f62dab72ca2e0
Alim-Reza <aarasif2@gmail.com>
2023-11-15 11:57:00 +0600

    1 file modified

--- src/ListWithDynamicFooter/ListWithDynamicFooter.jsx
+++ src/ListWithDynamicFooter/ListWithDynamicFooter.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import './ListWithDynamicFooter.css';
 
-async function apiCall() {
+async function apiCall(pageNumber, pageSize = 10) {
   var requestOptions = {
     method: 'GET',
     redirect: 'follow',
   };
 
   const response = await fetch(
-    'https://api.alquran.cloud/v1/juz/30/quran-uthmani?offset=0&limit=15',
+    `https://api.alquran.cloud/v1/juz/30/quran-uthmani?offset=${pageNumber}&limit=${pageSize}`,
     requestOptions
   );
   return await response.json();
 }
 export default function ListWithDynamicFooter() {
   const [list, setList] = React.useState([]);
-  apiCall().then((response) => {
-    setList(response.data.ayahs);
-  });
+  const [page, setPage] = React.useState(0);
+  function triggerApiCall(page) {
+    apiCall(page).then((response) => {
+      setList((prev) => [...prev, ...response.data.ayahs]);
+    });
+  }
+  React.useEffect(() => {
+    triggerApiCall(page);
+  }, [page]);
   return (
     <>
       <div className="wrapper">
         <div className="container">
-          {list.map((x, key) => (
-            <div key={key}>{x.text}</div>
+          {list.map((x, index) => (
+            <div>
+              {index} {x.text}
+            </div>
           ))}
         </div>
       </div>
