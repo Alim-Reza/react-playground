36a59d373d8abd7f08272ef02ed700c581bfd44d
Alim-Reza <aarasif2@gmail.com>
2023-11-15 12:42:41 +0600

    3 files modified

--- src/ListWithDynamicFooter/ListWithDynamicFooter.jsx
+++ src/ListWithDynamicFooter/ListWithDynamicFooter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ListWithDynamicFooter.css';
 
-async function apiCall(pageNumber, pageSize = 10) {
+async function apiCall(pageNumber, pageSize) {
   var requestOptions = {
     method: 'GET',
     redirect: 'follow',
@@ -16,8 +16,9 @@ async function apiCall(pageNumber, pageSize = 10) {
 export default function ListWithDynamicFooter() {
   const [list, setList] = React.useState([]);
   const [page, setPage] = React.useState(0);
-  function triggerApiCall(page) {
-    apiCall(page).then((response) => {
+  const [size, setSize] = React.useState(5);
+  function triggerApiCall(page, size) {
+    apiCall(page, size).then((response) => {
       setList((prev) => [...prev, ...response.data.ayahs]);
     });
   }
@@ -25,15 +26,30 @@ export default function ListWithDynamicFooter() {
     setPage((prev) => prev + 1);
   }
   React.useEffect(() => {
-    triggerApiCall(page);
+    triggerApiCall(page, size);
   }, [page]);
+  React.useEffect(() => {
+    setList([]);
+    setPage(0);
+    triggerApiCall(page, size);
+  }, [size]);
   return (
     <>
+      <label for="pageSize">Page Size:</label>
+      <select name="pageSize" id="pageSize">
+        {[...Array(10).keys()].map((i) => (
+          <option value={i} onClick={() => alert(i)}>
+            {i}
+          </option>
+        ))}
+      </select>
+
       <div className="wrapper">
         <div className="container">
           {list.map((x, index) => (
             <div>
-              {index + 1}. {x.text}
+              {index + 1}. {x.numberInSurah}
+              {/* {x.text} */}
             </div>
           ))}
           <button className="btn" onClick={onclk}>
